862a296f6a336328d2ff000021ad6559b7cb95fd
Rutuza <dalal.jyotika05@gmail.com>
2024-05-10 11:55:12 +0530

    create session btn separated

---
+++ src/components/Buttons/CreateSessionBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CreateSessionBtn() {
+  return (
+    <>
+      <button type="button" className='w-full flex items-center justify-center gap-8 bg-pastel_blue p-3 rounded-md text-white font-semibold text-base'>
+        <div>Create Session</div>
+        <div>+</div>
+      </button>
+    </>
+  );
+}
+
+export default CreateSessionBtn;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,51 +6,49 @@ import { IoMdBook } from "react-icons/io";
 import { TfiWrite } from "react-icons/tfi";
 import { RiLiveLine } from "react-icons/ri";
 import { BsPersonCheck } from "react-icons/bs";
+import CreateSessionBtn from './Buttons/CreateSessionBtn';
 
 function Sidebar() {
   return (
-    <div className='relative bg-light_navy w-60 h-full text-white flex flex-col justify-between p-2'>
+    <div className='w-60 '>
       <div className='text-3xl font-medium'>Codaf<span className='text-[#FF4773]'>fix</span></div>
       
-      <ul className='text-slate_blue'>
-        <li className='p-3 hover:bg-pastel_pink w-full hover:rounded-md'>
-          <Link to="/" className='flex items-center gap-3 hover:text-white'>
-            <i><RxDashboard /></i>
-            <span>Dashboard</span>
-          </Link>
-        </li>
-        <li className='p-3 hover:bg-pastel_pink w-full hover:rounded-md'>
-          <Link to="mycourses" className='flex items-center gap-3 hover:text-white'>
-            <i><IoMdBook /></i>
-            <span>My courses</span>
-          </Link>
-        </li>
-        <li className='p-3 hover:bg-pastel_pink w-full hover:rounded-md'>
-          <Link to="assignments" className='flex items-center gap-3 hover:text-white'>
-            <i><TfiWrite /></i>
-            <span>Assignments</span>
-          </Link>
-        </li>
-        <li className='p-3 hover:bg-pastel_pink w-full hover:rounded-md'>
-          <Link to="discussions" className='flex items-center gap-3 hover:text-white'>
-            <i><RiLiveLine /></i>
-            <span>Discussion</span>
-          </Link>
-        </li>
-        <li className='p-3 hover:bg-pastel_pink w-full hover:rounded-md'>
-          <Link to="attendance" className='flex items-center gap-3 hover:text-white'>
-            <i><BsPersonCheck /></i>
-            <span>Attendance</span>
-          </Link>
-        </li>
-      </ul>
+      <div className='py-3'>
+        <ul className='text-slate_blue grid grid-flow-row items-center justify-center gap-4 w-full'>
+          <li className='hover:bg-pastel_pink w-full hover:rounded-md'>
+            <Link to="/" className='flex items-center gap-3 hover:text-white'>
+              <i><RxDashboard /></i>
+              <span>Dashboard</span>
+            </Link>
+          </li>
+          <li className='hover:bg-pastel_pink w-full hover:rounded-md'>
+            <Link to="mycourses" className='flex items-center gap-3 hover:text-white'>
+              <i><IoMdBook /></i>
+              <span>My courses</span>
+            </Link>
+          </li>
+          <li className='hover:bg-pastel_pink w-full hover:rounded-md'>
+            <Link to="assignments" className='flex items-center gap-3 hover:text-white'>
+              <i><TfiWrite /></i>
+              <span>Assignments</span>
+            </Link>
+          </li>
+          <li className='hover:bg-pastel_pink w-full hover:rounded-md'>
+            <Link to="discussions" className='flex items-center gap-3 hover:text-white'>
+              <i><RiLiveLine /></i>
+              <span>Discussion</span>
+            </Link>
+          </li>
+          <li className='hover:bg-pastel_pink w-full hover:rounded-md'>
+            <Link to="attendance" className='flex items-center gap-3 hover:text-white'>
+              <i><BsPersonCheck /></i>
+              <span>Attendance</span>
+            </Link>
+          </li>
+        </ul>
 
-      <button type="button" className='border-2 w-full h-16 rounded-xl bg-pastel_blue'>
-        <div className='flex items-center gap-3 justify-center text-lg'>
-          <div>Create Session</div>
-          <i>+</i>
-        </div>
-      </button>
+        <CreateSessionBtn/>
+      </div>
     </div>
   );
 }
