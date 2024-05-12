f07da97d9b0cf38ebdd6f57a4997fb9354b0044e
Rutuza <dalal.jyotika05@gmail.com>
2024-05-13 00:49:15 +0530

    Co-authored-by: Adarsh Rathore <AdarshRathore223@users.noreply.github.com> sidebar done

--- src/Component/Sidebar.tsx
+++ src/Component/Sidebar.tsx
@@ -1,21 +1,54 @@
 import React from "react";
+import { Button } from "./ui/button";
+
+import { RxDashboard } from "react-icons/rx";
+import { IoBookOutline } from "react-icons/io5";
+import { TfiWrite } from "react-icons/tfi";
+import { BsCameraVideo } from "react-icons/bs";
+import { LuUserCheck2 } from "react-icons/lu";
 
 function Sidebar() {
+  const Navlinks = [
+    {
+      title: "Dashboard",
+      icon: <RxDashboard />,
+    },
+    {
+      title: "My Courses",
+      icon: <IoBookOutline />,
+    },
+    {
+      title: "Assignments",
+      icon: <TfiWrite />,
+    },
+    {
+      title: "Discussions",
+      icon: <BsCameraVideo />,
+    },
+    {
+      title: "Discussions",
+      icon: <LuUserCheck2 />,
+    },
+  ];
+
   return (
     <>
-      <nav className="bg-blue-300 w-60 h-96 flex flex-col items-center justify-between">
-        <div className="text-xl font-bold">Codaffix</div>
-        <ul>
-          <li>Dashboard</li>
-          <li>My courses</li>
-          <li>Assignments</li>
-          <li>Discussions</li>
-          <li>Attendance</li>
+      <div className="w-60 h-screen bg-darkprimary flex flex-col overflow-hidden text-white justify-between items-center fixed">
+        <h1 className="text-2xl font-medium">Codeaffix</h1>
+        <ul className="w-full">
+          {Navlinks.map((item, index) => (
+            <li key={index} className="p-2 hover:bg-pink-600 w-full">
+              <div className="flex items-center">
+                {item.icon}
+                {item.title}
+              </div>
+            </li>
+          ))}
         </ul>
-        <button type="button">Create Session</button>
-      </nav>
+        <Button className="bg-pastelblue w-[80%] m-4">Create Session</Button>
+      </div>
     </>
   );
 }
 
-export default Sidebar;
+ Sidebar;
--- src/Layout/Dashboard.tsx
+++ src/Layout/Dashboard.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Component/Sidebar";
 
-function Dashboard() {
+export default function Dashboard() {
   return (
-    <>
-      <Outlet />
-      <Sidebar />
-    </>
+    <div className="bg-gray-100 grid md:grid-cols-10">
+      <div className="col-span-2">
+        <Sidebar />
+      </div>
+      <div className="h-[200vh]">
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
-export default Dashboard;
