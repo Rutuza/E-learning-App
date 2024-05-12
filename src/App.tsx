df735edab7e381876c554238ddff70caf64929d5
Rutuza <dalal.jyotika05@gmail.com>
2024-05-12 15:14:37 +0530

    sidebar initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import Dashboard from './Layout/Dashboard';
-import Home from './Pages/Home';
+// import Dashboard from './Layout/Dashboard';
+// import Home from './Pages/Home';
 import './index.css'
+import demoicon from './assets/svg/demoicon.svg';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Router>
+    <div className="flex">
+      <div className={`w-72 h-screen bg-violet-950 text-white p-5 pt-8`}>
+        <img src="" alt="" />
+
+        <div className='flex items-center gap-x-4'>
+          <img src={demoicon} alt="" />
+          <h1 className={`text-white origin-left font-semibold text-xl`}>Coderoom</h1>
+        </div>
+      </div>
+      <div className='p-7 text-2xl font-semibold flex-1 h-screen duration-300'><h1>Home Page</h1></div>
+    </div>
+      {/* <Router>
         <Routes>
           <Route path='/' element={<Dashboard/>}>
             <Route path='home' element={<Home/>}>
@@ -16,7 +28,7 @@ function App() {
             <Route path='contact' element={<div>hi</div>}></Route>
           </Route>
         </Routes>
-      </Router>
+      </Router> */}
     </>
   );
 }
--- src/Component/Sidebar.tsx
+++ src/Component/Sidebar.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Sidebar() {
   return (
     <>
-      
+      <nav className="bg-blue-300 w-60 h-96 flex flex-col items-center justify-between">
+        <div className="text-xl font-bold">Codaffix</div>
+        <ul>
+          <li>Dashboard</li>
+          <li>My courses</li>
+          <li>Assignments</li>
+          <li>Discussions</li>
+          <li>Attendance</li>
+        </ul>
+        <button type="button">Create Session</button>
+      </nav>
     </>
   );
 }
